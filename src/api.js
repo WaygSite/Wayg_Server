99657ce0c16eca2f5b27be806ec38f64cdea441e
YJH2848 <yunj05763@gmail.com>
2023-02-03 02:41:41 +0900

    BUG : 아무리 봐도 왜 db에 데이터가 안드가는지 모르겠어;;;

--- src/api.js
+++ src/api.js
@@ -1,46 +1,19 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const axios = require("axios");
-// const port = 8000;
-
-// app.use(cors());
-
-// app.get("/", (req, res) => {
-//   let response = null;
-//   try {
-//     let api = async () => {
-//       response = axios.get(
-//         "https://api.odcloud.kr/api/15050724/v1/uddi:1c79c18a-ef4e-40a7-8b42-df1f94f15513_201711161619?page=1&perPage=10&returnType=XML",
-//         {
-//           params: {
-//             serviceKey:
-//               "38E/Z4yURCT3H1kNL0fgSZRQRrbZEyoeujrEmRvGZgEe8V2c9OrDf+sgfjqT6JrLzQOSpvGfbm0D/diK1Z0sqA==",
-//           },
-//         }
-//       );
-//     };
-//     api().then(() => {
-//       res.setHeader("Access-Control-Allow-Origin", "*");
-//     });
-//   } catch (e) {
-//     console.log("error!!");
-//   }
-
-//   return response;
-// });
-
-// app.listen(port, () => {
-//   console.log("get Start Server!!");
-// });
-
-//--------------------------------
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
 const port = 3001;
+
+const mysql = require("mysql2");
 require("dotenv").config({ path: "./.env" });
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
 
 app.use(cors());
 
@@ -54,8 +27,8 @@ app.get("/", async (req, res) => {
     });
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.json(response.data);
+
     response.data.data.forEach(function (school) {
-      //프론트에서 가져오고 싶은 데이터들
       const Institution = school["설립구분"];
       const MaW = school["남녀공학구분"];
       const Type = school["고교유형"];
@@ -63,18 +36,32 @@ app.get("/", async (req, res) => {
       const Addr = school["학교도로명주소"];
       const PhNum = school["학교전화번호"];
       const FoundDate = school["설립일자"];
-      //forEach를 사용하여 api데이터 안에 있는 json파일 중 "학교"라는 파라미터들을 전부 뽑아낸다
-      console.log("설립구분 : " + Institution);
-      console.log("남녀공학구분 : " + MaW);
-      console.log("고교유형 : " + Type);
-      console.log("학교 : " + SchoolName);
-      console.log("학교주소 : " + Addr);
-      console.log("학교번호 : " + PhNum);
-      console.log("설립일자 : " + FoundDate);
-      console.log("-----------------------");
+
+      connection.connect();
+      const query =
+        "INSERT INTO schoolinfo" +
+        "(Institution, MaW, Type, SchoolName, Addr, PhNum, FoundDate)" +
+        "VALUES (?,?,?,?,?,?,?)";
+      const values = [
+        Institution,
+        MaW,
+        Type,
+        SchoolName,
+        Addr,
+        PhNum,
+        FoundDate,
+      ];
+
+      connection.query(query, values, function (error, results) {
+        if (error) {
+          console.error(error);
+        }
+        console.log(results);
+        connection.end();
+      });
     });
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 });
 
