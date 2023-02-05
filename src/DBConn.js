e905ce3b43d622a692652e5c40bc3a91fcc2966e
YJH2848 <yunj05763@gmail.com>
2023-02-06 06:27:14 +0900

    CHORE : MYSQL을 이용하여 API를 만듦

---
+++ src/DBConn.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const port = 3000;
+require("dotenv").config({ path: "./.env" });
+const con = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+con.connect((err) => {
+  if (err) console.log("MySQL 연결 실패 : ", err);
+  console.log("MySQL Connected!!!");
+});
+app.get("/api/schoolinfo", function (req, res) {
+  con.query("SELECT * FROM schoolinfo", function (err, rows) {
+    if (err) throw err;
+
+    res.send(rows);
+  });
+});
+
+app.listen(port, () => console.log("Server Running. . ."));
